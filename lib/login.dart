95b5b19600843d554bd7eff602bd3b70bcbc29d8
sanjaygangwar <dowqix-qostam-gotTy0>
2021-10-23 01:36:38 +0530

    Made UI for login

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,45 +18,105 @@ class _LoginState extends State<Login> {
         body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/images/splash_background.png"),
+              image: AssetImage("assets/images/login.png"),
               fit: BoxFit.cover,
             ),
           ),
           child: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: const <Widget>[
-                SizedBox(
-                  height: 30,
+                Center(
+                  child: Text(
+                    "GET STARTED",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                Text(
-                  "GET STARTED",
-                  style: TextStyle(color: Colors.white, fontSize: 25,fontWeight: FontWeight.bold),
+                SizedBox(
+                  height: 20,
                 ),
                 SizedBox(
-                    height: 200,
-                    width: 150,
-                    child: Image(
-                      image: AssetImage('assets/images/phone.png'),
-                    )),
+                  height: 200,
+                  width: 150,
+                  child: Image(
+                    image: AssetImage('assets/images/phone.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
+                  padding: EdgeInsets.all(8.0),
+                  child: Card(
+                    elevation: 10,
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
                         focusedBorder: OutlineInputBorder(
                           borderSide:
                               BorderSide(color: Colors.white, width: 2.0),
                         ),
                         disabledBorder: OutlineInputBorder(
                           borderSide:
-                          BorderSide(color: Colors.white, width: 2.0),
-                        ),
-                        icon: Icon(
-                          Icons.phone,
-                          color: Colors.white,
+                              BorderSide(color: Colors.black, width: 2.0),
                         ),
                         border: OutlineInputBorder(),
-                        hintText: 'Mobile',
-                        hintStyle: TextStyle(color: Colors.white)),
+                        hintText: 'ENTER MOBILE NUMBER',
+                        hintStyle: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Card(
+                    elevation: 10,
+                    child: InkWell(
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Center(
+                          child: Text(
+                            "CONTINUE",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Text(
+                    "OR",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 10,
+                    child: InkWell(
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Center(
+                          child: Text(
+                            "TRY OUR APP",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
